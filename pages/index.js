3c2bd24d7858a6da99df3fc10adf197e102d768d
nleonn <nikoleonn@outlook.com>
2022-02-04 10:22:48 -0300

    third commit

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
 export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
     <Box>
-      <h1>Hello World</h1>
       <Banner 
         purpose="RENT A HOME"
         title1="Rental Homes For"
